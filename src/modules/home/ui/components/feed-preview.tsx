304474d36b496a71dd423e9da70f47767890675b
Manuel Lippe <mail@lippe-mann.de>
2026-05-04 13:09:35 +0200

    feat(preview): better visual separation to other posts

--- src/modules/home/ui/components/feed-preview.tsx
+++ src/modules/home/ui/components/feed-preview.tsx
@@ -44,18 +44,30 @@ export const FeedPreview = ({ excludeSlug, limit = 3 }: FeedPreviewProps) => {
   }
 
   return (
-    <div className='w-full pt-8 space-y-12 max-w-420 mx-auto relative'>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 bg-muted gap-3 md:gap-[0.06rem]  border-muted  border-y-12 md:border-y -mx-3 md:mx-0'>
-        {filteredPosts.map((post, i) => (
-          <PostCard key={post.id} post={post} index={i} />
-        ))}
+    <>
+      <div className='bg-muted md:bg-background  -mx-3  pt-12 pb-10 md:py-16 flex gap-5 items-center md:justify-center'>
+        <div className='h-0.5 w-full bg-border md:hidden' />
+        <h3 className='text-foreground font-medium text-xl md:text-2xl w-fit whitespace-nowrap text-center'>
+          Weitere Einträge
+        </h3>
+        <div className='h-0.5 w-full bg-border md:hidden' />
       </div>
-      <div className='absolute bottom-0 -left-3 w-[calc(100%+1.5rem)] h-[75vh] md:h-1/2  z-50 flex flex-col gap-4 justify-center items-center pb-10 bg-linear-to-b from-background/0 via-background/90 to-background via-40% to-80%  '>
-        <p className='text-foreground font-medium text-xl'>Du willst mehr?</p>
-        <Button asChild className='rounded-full px-8 uppercase tracking-widest'>
-          <Link href='/'>Alle Einträge ansehen</Link>
-        </Button>
+      <div className='w-full space-y-12 max-w-420 mx-auto relative '>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 bg-muted gap-3 md:gap-[0.06rem]  border-muted  border-y-12 md:border-y -mx-3 md:mx-0'>
+          {filteredPosts.map((post, i) => (
+            <PostCard key={post.id} post={post} index={i} />
+          ))}
+        </div>
+        <div className='absolute bottom-0 -left-3 w-[calc(100%+1.5rem)] h-[75vh] md:h-1/2  z-50 flex flex-col gap-4 justify-center items-center pb-10 bg-linear-to-b from-background/0 via-background/90 to-background via-40% to-80%  '>
+          <p className='text-foreground font-medium text-xl'>Du willst mehr?</p>
+          <Button
+            asChild
+            className='rounded-full px-8 uppercase tracking-widest'
+          >
+            <Link href='/'>Alle Einträge ansehen</Link>
+          </Button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
