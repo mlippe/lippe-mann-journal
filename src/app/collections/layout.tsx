82facece6f28784bfe125e988f01989df0e9b994
Manuel Lippe <mail@lippe-mann.de>
2026-05-15 15:23:11 +0200

    fix(collections): build error

--- src/app/collections/layout.tsx
+++ src/app/collections/layout.tsx
@@ -1,20 +1,13 @@
 import Header from '@/modules/home/ui/components/header';
-import type { PropsWithChildren, ReactNode } from 'react';
+import type { PropsWithChildren } from 'react';
 
-export type HomeLayoutProps = Readonly<
-  PropsWithChildren<{
-    modals: ReactNode;
-  }>
->;
-
-const HomeLayout = async ({ children, modals }: HomeLayoutProps) => {
+const CollectionLayout = async ({ children }: PropsWithChildren) => {
   return (
     <>
       <Header />
       <main className='h-screen p-3'>{children}</main>
-      {modals}
     </>
   );
 };
 
-export default HomeLayout;
+export default CollectionLayout;
